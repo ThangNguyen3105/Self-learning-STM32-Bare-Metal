d8187aaa73b3137812c2a6c4ff4d31078234fbc6
Nguyen Hoang Thang <tn133931@gmail.com>
2025-06-26 23:29:41 +0700

    i2c_init_completed

--- i2c/Inc/i2c.h
+++ i2c/Inc/i2c.h
@@ -12,6 +12,8 @@
 
 #define I2C1EN				(1U<<21)
 #define IOPBEN				(1U<<3)
+
+// PB6
 #define CRL_CNF6_POS		(26U)
 #define CRL_CNF6_MASK		(3U<<CRL_CNF6_POS)
 #define CRL_CNF6			CRL_CNF6_MASK
@@ -24,9 +26,26 @@
 #define CRL_MODE6_0			(1U<<CRL_MODE6_POS)
 #define CRL_MODE6_1			(2U<<CRL_MODE6_POS)
 
+// PB7
+#define CRL_CNF7_POS		(30U)
+#define CRL_CNF7_MASK		(3U<<CRL_CNF7_POS)
+#define CRL_CNF7			CRL_CNF7_MASK
+#define CRL_CNF7_0			(1U<<CRL_CNF7_POS)
+#define CRL_CNF7_1			(2U<<CRL_CNF7_POS)
+
+#define CRL_MODE7_POS		(28U)
+#define CRL_MODE7_MASK		(3U<<CRL_MODE7_POS)
+#define CRL_MODE7			CRL_MODE7_MASK
+#define CRL_MODE7_0			(1U<<CRL_MODE7_POS)
+#define CRL_MODE7_1			(2U<<CRL_MODE7_POS)
+
+#define CR1_PE_I2C			(1U<<0)
+
+#define CR1_START_I2C		(1U<<8)
+#define SR1_SB_I2C			(1U<<0)
 
 void i2c_init(void);
-
+void I2C1_ScanAddresses(void);
 
 
 #endif /* I2C_H_ */
--- i2c/Src/i2c.c
+++ i2c/Src/i2c.c
@@ -13,4 +13,44 @@ void i2c_init(void){
 	GPIOB->CRL |= (CRL_CNF6_0 | CRL_CNF6_1);
 	// set MODE6: 11 to configure max speed 50 MHz
 	GPIOB->CRL |= (CRL_MODE6_0 | CRL_MODE6_1);
+
+	// PB7
+	// clear 2 bit CNF, MODE
+	GPIOB->CRL &= ~(CRL_CNF7_MASK | CRL_MODE7_MASK);
+	// set CNF7: 11 to configure AF open-drain
+	GPIOB->CRL |= (CRL_CNF7_0 | CRL_CNF7_1);
+	// set MODE7: 11 to configure max speed 50 MHz
+	GPIOB->CRL |= (CRL_MODE7_0 | CRL_MODE7_1);
+
+	// Enable i2c
+	I2C1->CR1 &= ~(CR1_PE_I2C);	// Disable before configure
+
+	I2C1->CR2 = 8;		// PCLK1 = 8 MHz set CR2[5:0] = 8
+	I2C1->CCR = 40;		// CCR = (f PCLK1)/(2*f SCL);
+						//f PCLK1 is frequency of bus APB1 = 8 MHz, f SCL = 100 kHz
+	I2C1->TRISE = 9;	// (T_rise / T_PCLK1) + 1
+
+	// Enable i2c
+	I2C1->CR1 |= (CR1_PE_I2C);
+
 }
+void I2C1_ScanAddresses(void){
+	for(uint8_t address = 0x03; address <= 0x77; address++){
+		// 1. Generate START Condition
+		I2C1->CR1 |= CR1_START_I2C;
+		// Wait for SB flag = 1 to know START Condition is complete
+		while(!(I2C1->SR1 & SR1_SB_I2C));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
